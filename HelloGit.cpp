81a3ea69dbb2e82d026faa64a5fd9429885a3fbe
Jin <ktj9279@gmail.com>
2017-01-19 01:55:27 +0900

    HelloGit.cpp modified on local repository

--- HelloGit.cpp
+++ HelloGit.cpp
@@ -6,6 +6,7 @@ int main(int argc, char* argv[]) {
 	cout << "Hello new world";
 	cout << "Hello master branch";
 	cout << "Hello hotfix branch";
+	cout << "World on local repository";
 
 	return 0;
 }
